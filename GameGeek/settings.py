46a37d29b34ea63f7f59dc2e8194598e2175f37e
Hana Alfozan <ihana2af@gmail.com>
2024-05-18 18:06:30 +0300

    deploy minor changes

--- GameGeek/settings.py
+++ GameGeek/settings.py
@@ -96,9 +96,9 @@ WSGI_APPLICATION = 'GameGeek.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'GameGeek',
-        #'USER': 'group5',
-        'USER': 'postgres',
+        'NAME': 'gamegeek',
+        'USER': 'group5',
+        #'USER': 'postgres',
         'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': '',
